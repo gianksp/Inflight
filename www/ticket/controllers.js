f3d5ada1ea7dad441f427b368a199d163358dc16
Giancarlo Sanchez <giancarlosanchez@Giancarlos-MacBook-Pro.local>
2015-03-26 12:27:56 +0000

    List

--- www/ticket/controllers.js
+++ www/ticket/controllers.js
@@ -50,4 +50,118 @@ angular.module('inflightApp.ticket').controller('EventListController', ['$scope'
             'background-size' : 'cover'
         });
     };
-});;
+})
+
+.directive('autoListDivider', function($timeout) {  
+	var lastDivideKey = "";
+
+	return {
+		link: function(scope, element, attrs) {
+			var key = attrs.autoListDividerValue;
+
+			var defaultDivideFunction = function(k){
+				return k;
+			}
+      
+			var doDivide = function(){
+				var divideFunction = scope.$apply(attrs.autoListDividerFunction) || defaultDivideFunction;
+				var divideKey = divideFunction(key);
+				
+				if(divideKey != lastDivideKey) { 
+					var contentTr = angular.element("<div class='item item-divider ticket-section'>"+divideKey+"</div>");
+					element[0].parentNode.insertBefore(contentTr[0], element[0]);
+				}
+
+				lastDivideKey = divideKey;
+			}
+		  
+			$timeout(doDivide,0)
+		}
+	}
+})
+
+ 
+.directive('affixWithinContainer', function($document, $ionicScrollDelegate) {
+ 
+  var transition = function(element, dy, executeImmediately) {
+    element.style[ionic.CSS.TRANSFORM] == 'translate3d(0, -' + dy + 'px, 0)' ||
+    executeImmediately ?
+    element.style[ionic.CSS.TRANSFORM] = 'translate3d(0, -' + dy + 'px, 0)' :
+    ionic.requestAnimationFrame(function() {
+      element.style[ionic.CSS.TRANSFORM] = 'translate3d(0, -' + dy + 'px, 0)';
+    });
+  };
+ 
+  return {
+    restrict: 'A',
+    require: '^$ionicScroll',
+    link: function($scope, $element, $attr, $ionicScroll) {
+      var $affixContainer = $element.closest($attr.affixWithinContainer) || $element.parent();
+ 
+      var top = 0;
+      var height = 0;
+      var scrollMin = 0;
+      var scrollMax = 0;
+      var scrollTransition = 0;
+      var affixedHeight = 0;
+      var updateScrollLimits = _.throttle(function(scrollTop) {
+          top = $affixContainer.offset().top;
+          height = $affixContainer.outerHeight(false);
+          affixedHeight = $element.outerHeight(false);
+          scrollMin = scrollTop + top;
+          scrollMax = scrollMin + height;
+          scrollTransition = scrollMax - affixedHeight;
+      }, 500, {
+          trailing: false
+      });
+ 
+      var affix = null;
+      var unaffix = null;
+      var $affixedClone = null;
+      var setupAffix = function() {
+          unaffix = null;
+          affix = function() {
+              $affixedClone = $element.clone().css({
+                  position: 'fixed',
+                  top: 0,
+                  left: 0,
+                  right: 0
+              });
+              $($ionicScroll.element).append($affixedClone);
+ 
+              setupUnaffix();
+          };
+      };
+      var cleanupAffix = function() {
+          $affixedClone && $affixedClone.remove();
+          $affixedClone = null;
+      };
+      var setupUnaffix = function() {
+          affix = null;
+          unaffix = function() {
+              cleanupAffix();
+              setupAffix();
+          };
+      };
+      $scope.$on('$destroy', cleanupAffix);
+      setupAffix();
+ 
+      var affixedJustNow;
+      var scrollTop;
+      $($ionicScroll.element).on('scroll', function(event) {
+        scrollTop = (event.detail || event.originalEvent && event.originalEvent.detail).scrollTop;
+        updateScrollLimits(scrollTop);
+        if (scrollTop >= scrollMin && scrollTop <= scrollMax) {
+            affixedJustNow = affix ? affix() || true : false;
+            if (scrollTop > scrollTransition) {
+                transition($affixedClone[0], Math.floor(scrollTop-scrollTransition), affixedJustNow);
+            } else {
+                transition($affixedClone[0], 0, affixedJustNow);
+            }
+        } else {
+            unaffix && unaffix();
+        }
+      });
+    }
+  }
+});
