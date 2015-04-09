c5a158a9092c5d72d3c6fe50aad8360601cd998d
Giancarlo Sanchez <giancarlosanchez@Giancarlos-MacBook-Pro.local>
2015-04-09 16:41:10 +0100

    Latest on styles

--- www/cityhook/controllers.js
+++ www/cityhook/controllers.js
@@ -7,9 +7,9 @@
  * @param  {[type]} Event) {}]         [description]
  * @return {[type]}        [description]
  */
-angular.module('inflightApp.cityhook')
+ angular.module('inflightApp.cityhook')
 
-/*********************************************************    CONTROLLERS    **************************************************************/
+ /*********************************************************    CONTROLLERS    **************************************************************/
 
 /**
  * Master view controller. Handles behavior of list of events, featured events, etc
@@ -18,7 +18,7 @@ angular.module('inflightApp.cityhook')
  * @param  {Array}  Event) {                   $scope.events [description]
  * @return {[type]}        [description]
  */
-.controller('NavController', ['$scope','$http','$state','$rootScope',
+ .controller('NavController', ['$scope','$http','$state','$rootScope',
   function($scope,$http,$state,$rootScope) {
 
     //Default layout tablet
@@ -32,7 +32,7 @@ angular.module('inflightApp.cityhook')
      * Ticketmaster - Cityhook
      * @return {[type]} [description]
      */
-    $scope.switchModule = function() {
+     $scope.switchModule = function() {
       if ($state.current.name == 'list') {
         $state.go('welcome');
         $scope.moduleTitle = "Load Ticketmaster";
@@ -46,13 +46,13 @@ angular.module('inflightApp.cityhook')
      * Switch between tablet - mobile
      * @return {[type]} [description]
      */
-    $scope.switchLayout = function() {
+     $scope.switchLayout = function() {
       $rootScope.layoutTablet = !$rootScope.layoutTablet;
       $scope.tablet = $rootScope.layoutTablet;
       $scope.layoutTitle = $scope.tablet ? "Load Mobile" : "Load Tablet";
     }
 
-}])
+  }])
 
 /**
  * Master view controller. Handles behavior of list of events, featured events, etc
@@ -61,59 +61,93 @@ angular.module('inflightApp.cityhook')
  * @param  {Array}  Event) {                   $scope.events [description]
  * @return {[type]}        [description]
  */
-.controller('WelcomeController', ['$scope','$compile','$http','$state','$ionicModal','$ionicPopup','Event','City','$state','$stateParams','$ionicPopover',
-  function($scope,$compile,$http,$state,$ionicModal,$ionicPopup, Event, City,$state,$stateParams,$ionicPopover) {
+ .controller('WelcomeController', ['$rootScope', '$scope','$compile','$http','$state','$ionicModal','$ionicPopup','Event','City','$state','$stateParams','$ionicPopover',
+  function($rootScope, $scope,$compile,$http,$state,$ionicModal,$ionicPopup, Event, City,$state,$stateParams,$ionicPopover) {
+
+    $scope.selectedTransport = null;
+
+    $scope.selectTransport = function(transport) {
+      $scope.selectedTransport = transport;
+      $scope.step = 2;
+      console.log("new step "+$scope.step);
+    }
+
+    $scope.transports = [];
+
+    $scope.formatDate = function(item) {
+      if (item == null || item == undefined) {
+        return "anytime";
+      } else {
+        return item.getDate()+"/"+item.getMonth()+" "+item.getHours()+":"+item.getMinutes();
+      }
+    }
+
+    $scope.step = 1;
+
+    $rootScope.$watch('layoutTablet', function() {
+      console.log("changed");
+      $scope.tablet = $rootScope.layoutTablet;
+
+    });
 
     //Popover content info
     $scope.popoverTitle = null;
-    $scope.currentSelection = null;
     $scope.popoverContent = [
-      { id : 1, name: "Paddington"},
-      { id : 1, name: "Victoria Station"},
-      { id : 1, name: "West End"},
-      { id : 1, name: "Westminster"},
-      { id : 1, name: "Chelsea"}
+    { id : 1, name: "Paddington", lat:51.516674 ,lon:-0.176933},
+    { id : 1, name: "Victoria Station", lat:51.49521, lon:-0.143898},
+    { id : 1, name: "West End", lat:51.2406292,lon:-0.565304},
+    { id : 1, name: "Westminster", lat:51.501272,lon:-0.124941},
+    { id : 1, name: "Chelsea", lat:51.4850925,lon:-0.174936}
     ];
 
+    $scope.currentSelection = $scope.popoverContent[0];
 
-$scope.statusCode = $stateParams.statusCode;
-console.log($scope.statusCode);
+    $scope.tablet = $rootScope.layoutTablet;
 
- $scope.initialize = function() {
-    var myLatlng = new google.maps.LatLng(43.07493,-89.381388);
-    
-    var mapOptions = {
-      center: myLatlng,
-      zoom: 16,
-      mapTypeId: google.maps.MapTypeId.ROADMAP
-    };
-    var map = new google.maps.Map(document.getElementById("map"),
+
+    console.log($scope.tablet);
+
+    $scope.statusCode = $stateParams.statusCode;
+    console.log($scope.statusCode);
+
+    $scope.initialize = function() {
+
+      
+      var myLatlng = new google.maps.LatLng($scope.currentSelection.lat,$scope.currentSelection.lon);
+      
+      var mapOptions = {
+        center: myLatlng,
+        zoom: 16,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+      };
+      var map = new google.maps.Map(document.getElementById("map"),
         mapOptions);
 
 
-    var marker = new google.maps.Marker({
-      position: myLatlng,
-      map: map,
-      title: 'Uluru (Ayers Rock)'
-    });
+      var marker = new google.maps.Marker({
+        position: myLatlng,
+        map: map,
+        title: 'Uluru (Ayers Rock)'
+      });
 
-    google.maps.event.addListener(marker, 'click', function() {
-      infowindow.open(map,marker);
-    });
+      google.maps.event.addListener(marker, 'click', function() {
+        infowindow.open(map,marker);
+      });
 
-    $scope.map = map;
-  }
+      $scope.map = map;
+    }
 
 
-  $scope.alert = function(){
-  alert('alerted!');
-};
+    $scope.alert = function(){
+      alert('alerted!');
+    };
   //google.maps.event.addDomListener(window, 'load', initialize);
 
 
   $scope.selectItem = function(item) {
     $scope.currentSelection = item;
     $scope.closePopover();
+    $scope.applySearchFilter();
   }
 
   $scope.customState = function(){
@@ -132,6 +166,10 @@ console.log($scope.statusCode);
     $scope.popover = popover;
   });
 
+  $scope.pay = function() {
+    $scope.step = 3;
+    console.log("payyyy");
+  }
 
   $scope.openPopover = function($event) {
     console.log($scope.popover);
@@ -154,7 +192,78 @@ console.log($scope.statusCode);
   });
 
 
+  $scope.applySearchFilter = function() {
+    $scope.step = 1;
+    console.log("filtering");
+    console.log("leaves "+$scope.leaves);
+    console.log("returns "+$scope.returns);
+    console.log("location "+$scope.currentSelection);
+    //Filter location
+    //filter leave date
+    //filter return date
+    
+    //We need to generate random events based on that
+    $scope.initialize();
 
+
+    //Generate
+    var counter = Math.floor((Math.random() * 4) + 1);
+    $scope.transports = [];
+    for (var i=0; i <counter ; i ++) {
+      $scope.transports.push({ name:"Premium Shuttle" , by: "Concierge Companion" ,time: "23min", price:80 , pic:"img/shuttle_premium.jpg"});
+      $scope.transports.push({ name:"Standard Shuttle" , by: "Silver Shuttle" ,time: "57min" ,price:45, pic:"img/shuttle_standard.jpg"});
+      $scope.transports.push({ name:"Heathrow Express" , by: "London Trains" ,time: "23min" ,price:75, pic:"img/heathrow_express.jpg"});
+    }
+
+  }
+
+    
+  $scope.leavePicker = function() {
+    $scope.tmp = {};
+    console.log("leaves");
+    var birthDatePopup = $ionicPopup.show({
+     template: '<datetimepicker data-datetimepicker-config="{ startView:\'hour\', minView:\'minute\' }" ng-model="tmp.leaves"></datetimepicker>',
+     title: "Select",
+     scope: $scope,
+     buttons: [
+       { text: 'Cancel' },
+       {
+         text: '<b>Search</b>',
+         type: 'button-positive',
+         onTap: function(e) {
+           $scope.leaves = $scope.tmp.leaves;
+           console.log($scope.leaves);
+           $scope.applySearchFilter();
+         }
+       }
+     ]
+    });
+  }
+
+    $scope.returnPicker = function() {
+    $scope.tmp = {};
+    
+    var birthDatePopup = $ionicPopup.show({
+     template: '<datetimepicker data-datetimepicker-config="{ startView:\'hour\', minView:\'minute\' }" ng-model="tmp.returns"></datetimepicker>',
+     title: "Select",
+     scope: $scope,
+     buttons: [
+       { text: 'Cancel' },
+       {
+         text: '<b>Search</b>',
+         type: 'button-positive',
+         onTap: function(e) {
+           $scope.returns = $scope.tmp.returns;
+           console.log($scope.returns);
+           $scope.applySearchFilter();
+         }
+       }
+     ]
+    });
+  }
+
+
+  $scope.applySearchFilter();
 
 }])
 
@@ -167,10 +276,10 @@ console.log($scope.statusCode);
  * @param  {[type]} Event) {}]         [description]
  * @return {[type]}        [description]
  */
-.controller('RouteController', ['$scope','$http','$state','$stateParams','Event','$ionicPopover',
+ .controller('RouteController', ['$scope','$http','$state','$stateParams','Event','$ionicPopover',
   function($scope,$http,$state,$stateParams, Event,$ionicPopover) {
 
- $scope.initialize = function() {
+   $scope.initialize = function() {
     var myLatlng = new google.maps.LatLng(43.07493,-89.381388);
     
     var mapOptions = {
@@ -179,7 +288,7 @@ console.log($scope.statusCode);
       mapTypeId: google.maps.MapTypeId.ROADMAP
     };
     var map = new google.maps.Map(document.getElementById("map-detail"),
-        mapOptions);
+      mapOptions);
 
 
     var marker = new google.maps.Marker({
@@ -195,7 +304,7 @@ console.log($scope.statusCode);
     $scope.map = map;
   }
 
-    $scope.routeType = $stateParams.type;
+  $scope.routeType = $stateParams.type;
 
 
   $scope.popover = $ionicPopover.fromTemplate(template, {
@@ -245,11 +354,11 @@ console.log($scope.statusCode);
  * @param  {[type]} Event)       {               $scope.itemIndex [description]
  * @return {[type]}              [description]
  */
-.controller('RouteDetailController', ['$scope','$http','$ionicPopup','$state','$stateParams','Event',
+ .controller('RouteDetailController', ['$scope','$http','$ionicPopup','$state','$stateParams','Event',
   function($scope,$http,$ionicPopup,$state,$stateParams, Event) {
 
 
-}])
+  }])
 
 /**
  * Event Detail Venue info
@@ -260,15 +369,15 @@ console.log($scope.statusCode);
  * @param  {[type]} Event)       {               $scope.itemIndex [description]
  * @return {[type]}              [description]
  */
-.controller('PurchaseController', ['$scope','$http','$state','$stateParams','Event',
+ .controller('PurchaseController', ['$scope','$http','$state','$stateParams','Event',
   function($scope,$http,$state,$stateParams, Event) {
 
-  $scope.pay = function(){
-    $state.go('welcome',{ statusCode: true });
-  }
+    $scope.pay = function(){
+      $state.go('welcome',{ statusCode: true });
+    }
 
 
-}])
+  }])
 
 /**
  * Event Detail Artist info
@@ -279,7 +388,7 @@ console.log($scope.statusCode);
  * @param  {[type]} Event)       {               $scope.itemIndex [description]
  * @return {[type]}              [description]
  */
-.controller('EventArtistController', ['$scope','$http','$state','$stateParams','Event',
+ .controller('EventArtistController', ['$scope','$http','$state','$stateParams','Event',
   function($scope,$http,$state,$stateParams, Event) {
 
     $scope.itemIndex = $stateParams.event_index;
@@ -289,9 +398,9 @@ console.log($scope.statusCode);
       console.log($scope.selectedEvent);
     });
 
-}])
+  }])
 
-/*********************************************************    DIRECTIVES    **************************************************************/
+ /*********************************************************    DIRECTIVES    **************************************************************/
 
 
 /**
@@ -299,14 +408,14 @@ console.log($scope.statusCode);
  * @param  {[type]} ){                 return function(scope, element, attrs){        var url [description]
  * @return {[type]}     [description]
  */
-.directive('backImg', function(){
-    return function(scope, element, attrs){
-        var url = attrs.backImg;
-        element.css({
-            'background-image': 'url(' + url +')',
-            'background-size' : 'cover'
-        });
-    };
+ .directive('backImg', function(){
+  return function(scope, element, attrs){
+    var url = attrs.backImg;
+    element.css({
+      'background-image': 'url(' + url +')',
+      'background-size' : 'cover'
+    });
+  };
 })
 
 /**
@@ -314,32 +423,32 @@ console.log($scope.statusCode);
  * @param  {String} $timeout) {               var lastDivideKey [description]
  * @return {[type]}           [description]
  */
-.directive('autoListDivider', function($timeout) {  
-	var lastDivideKey = "";
+ .directive('autoListDivider', function($timeout) {  
+   var lastDivideKey = "";
 
-	return {
-		link: function(scope, element, attrs) {
-			var key = attrs.autoListDividerValue;
+   return {
+    link: function(scope, element, attrs) {
+     var key = attrs.autoListDividerValue;
 
-			var defaultDivideFunction = function(k){
-				return k;
-			}
+     var defaultDivideFunction = function(k){
+      return k;
+    }
+    
+    var doDivide = function(){
+      var divideFunction = scope.$apply(attrs.autoListDividerFunction) || defaultDivideFunction;
+      var divideKey = divideFunction(key);
       
-			var doDivide = function(){
-				var divideFunction = scope.$apply(attrs.autoListDividerFunction) || defaultDivideFunction;
-				var divideKey = divideFunction(key);
-				
-				if(divideKey != lastDivideKey) { 
-					var contentTr = angular.element("<div class='item item-divider ticket-section'>"+divideKey+"</div>");
-					element[0].parentNode.insertBefore(contentTr[0], element[0]);
-				}
+      if(divideKey != lastDivideKey) { 
+       var contentTr = angular.element("<div class='item item-divider ticket-section'>"+divideKey+"</div>");
+       element[0].parentNode.insertBefore(contentTr[0], element[0]);
+     }
 
-				lastDivideKey = divideKey;
-			}
-		  
-			$timeout(doDivide,0)
-		}
-	}
+     lastDivideKey = divideKey;
+   }
+   
+   $timeout(doDivide,0)
+ }
+}
 })
 
 /**
@@ -348,8 +457,8 @@ console.log($scope.statusCode);
  * @param  {[type]} $ionicScrollDelegate) {              var transition [description]
  * @return {[type]}                       [description]
  */
-.directive('affixWithinContainer', function($document, $ionicScrollDelegate) {
- 
+ .directive('affixWithinContainer', function($document, $ionicScrollDelegate) {
+   
   var transition = function(element, dy, executeImmediately) {
     element.style[ionic.CSS.TRANSFORM] == 'translate3d(0, -' + dy + 'px, 0)' ||
     executeImmediately ?
@@ -358,13 +467,13 @@ console.log($scope.statusCode);
       element.style[ionic.CSS.TRANSFORM] = 'translate3d(0, -' + dy + 'px, 0)';
     });
   };
- 
+  
   return {
     restrict: 'A',
     require: '^$ionicScroll',
     link: function($scope, $element, $attr, $ionicScroll) {
       var $affixContainer = $element.closest($attr.affixWithinContainer) || $element.parent();
- 
+      
       var top = 0;
       var height = 0;
       var scrollMin = 0;
@@ -372,61 +481,61 @@ console.log($scope.statusCode);
       var scrollTransition = 0;
       var affixedHeight = 0;
       var updateScrollLimits = _.throttle(function(scrollTop) {
-          top = $affixContainer.offset().top;
-          height = $affixContainer.outerHeight(false);
-          affixedHeight = $element.outerHeight(false);
-          scrollMin = scrollTop + top;
-          scrollMax = scrollMin + height;
-          scrollTransition = scrollMax - affixedHeight;
+        top = $affixContainer.offset().top;
+        height = $affixContainer.outerHeight(false);
+        affixedHeight = $element.outerHeight(false);
+        scrollMin = scrollTop + top;
+        scrollMax = scrollMin + height;
+        scrollTransition = scrollMax - affixedHeight;
       }, 500, {
-          trailing: false
+        trailing: false
       });
- 
+      
       var affix = null;
       var unaffix = null;
       var $affixedClone = null;
       var setupAffix = function() {
-          unaffix = null;
-          affix = function() {
-              $affixedClone = $element.clone().css({
-                  position: 'fixed',
-                  top: 0,
-                  left: 0,
-                  right: 0
-              });
-              $($ionicScroll.element).append($affixedClone);
- 
-              setupUnaffix();
-          };
+        unaffix = null;
+        affix = function() {
+          $affixedClone = $element.clone().css({
+            position: 'fixed',
+            top: 0,
+            left: 0,
+            right: 0
+          });
+          $($ionicScroll.element).append($affixedClone);
+          
+          setupUnaffix();
+        };
       };
       var cleanupAffix = function() {
-          $affixedClone && $affixedClone.remove();
-          $affixedClone = null;
+        $affixedClone && $affixedClone.remove();
+        $affixedClone = null;
       };
       var setupUnaffix = function() {
-          affix = null;
-          unaffix = function() {
-              cleanupAffix();
-              setupAffix();
-          };
+        affix = null;
+        unaffix = function() {
+          cleanupAffix();
+          setupAffix();
+        };
       };
       $scope.$on('$destroy', cleanupAffix);
       setupAffix();
- 
+      
       var affixedJustNow;
       var scrollTop;
       $($ionicScroll.element).on('scroll', function(event) {
         scrollTop = (event.detail || event.originalEvent && event.originalEvent.detail).scrollTop;
         updateScrollLimits(scrollTop);
         if (scrollTop >= scrollMin && scrollTop <= scrollMax) {
-            affixedJustNow = affix ? affix() || true : false;
-            if (scrollTop > scrollTransition) {
-                transition($affixedClone[0], Math.floor(scrollTop-scrollTransition), affixedJustNow);
-            } else {
-                transition($affixedClone[0], 0, affixedJustNow);
-            }
+          affixedJustNow = affix ? affix() || true : false;
+          if (scrollTop > scrollTransition) {
+            transition($affixedClone[0], Math.floor(scrollTop-scrollTransition), affixedJustNow);
+          } else {
+            transition($affixedClone[0], 0, affixedJustNow);
+          }
         } else {
-            unaffix && unaffix();
+          unaffix && unaffix();
         }
       });
     }
@@ -447,7 +556,7 @@ console.log($scope.statusCode);
           mapTypeId: google.maps.MapTypeId.ROADMAP
         };
         var map = new google.maps.Map($element[0], mapOptions);
-  
+        
         $scope.onCreate({map: map});
 
         // Stop the side bar from dragging when mousedown/tapdown on the map
